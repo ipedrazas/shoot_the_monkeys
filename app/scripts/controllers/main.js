de92aef8062a460631e812242d236144aae442d6
Ivan Pedrazas <ipedrazas@gmail.com>
2013-10-09 18:23:33 +0100

    players added by ret key, players persisted between views

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,38 +1,52 @@
+//
+
 'use strict';
 
-angular.module('MonkeysApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
-    $scope.players = [
-        'Ivan',
-        'Eloy',
-        'Antonio',
-        'Alex'
-    ];
+angular.module('MonkeysApp').factory('api', function(){
+        var players = [];
+        return{
+            addPlayer: function(player){
+                players.push(player);
+              },
+            getPlayers: function(){
+                return players;
+              }
+          };
+      });
 
-    $scope.monkeys = [
-      {
-        state: true
-      },
-      {
-        state: true
-      },
-      {
-        state: true
-      },
-      {
-        state: true
-      },
-    ];
+angular.module('MonkeysApp').controller('MainCtrl', function ($scope, api) {
+        $scope.players = api.getPlayers();
 
-    $scope.$on('makey', function(event, value) {
-      $scope.monkeys[value].state = !$scope.monkeys[value].state;
-      $scope.$apply();
-    });
+        $scope.addPlayer = function(player){
+            api.addPlayer(player);
+            $scope.newplayer = '';
+          };
 
-  });
+        $scope.monkeys = [
+          {
+            state: true
+          },
+          {
+            state: true
+          },
+          {
+            state: true
+          },
+          {
+            state: true
+          },
+        ];
+
+        $scope.$on('makey', function(event, value) {
+          $scope.monkeys[value].state = !$scope.monkeys[value].state;
+          $scope.$apply();
+        });
+
+        $scope.$on('ret_key', function(event, player) {
+             api.addPlayer($scope.newplayer);
+             $scope.newplayer = '';
+          $scope.$apply();
+        });
+
+      });
--- app/scripts/directives/body.js
+++ app/scripts/directives/body.js
@@ -1,30 +1,48 @@
-'use strict';
+        'use strict';
 
-angular.module('MonkeysApp')
-  .directive('body', function () {
+
+
+        function log(entry){
+        if( console && console.log ) {
+        console.log(entry);
+        }
+        }
+
+
+
+angular.module('MonkeysApp').directive('body', function () {
     return {
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.bind('keydown', function(event) {
+            restrict: 'E',
+            link: function postLink(scope, element, attrs) {
+                element.bind('keydown', function(event) {
 
-          var convertKeyCode = function(keyCode) {
-            switch(keyCode) {
-              case 37:
-                  return 0;
-              case 38:
-                  return 1;
-              case 39:
-                  return 2;
-              case 40:
-                  return 3;
-              default:
-                return 4;
-            }
-          };
-          var monkey = convertKeyCode(event.keyCode);
-          if (monkey !== 4)
-            scope.$broadcast('makey', monkey);
-        });
-      }
+                        var convertKeyCode = function(keyCode) {
+
+                        switch(keyCode) {
+                            case 13:
+                                return 5;
+                            case 37:
+                                return 0;
+                            case 38:
+                                return 1;
+                            case 39:
+                                return 2;
+                            case 40:
+                                return 3;
+                          default:
+                            return 4;
+                          }
+                        };
+                        var monkey = convertKeyCode(event.keyCode);
+                        if (monkey !== 4){
+                          scope.$broadcast('makey', monkey);
+                          if (monkey === 5){
+                            log('Return');
+                            scope.$broadcast('ret_key', monkey);
+                          }
+                        }
+
+                      });
+              }
     };
   });
