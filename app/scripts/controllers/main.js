22fc72094d50e5e1e2aac63f245665d2cf1d3b08
Ivan Pedrazas <ipedrazas@gmail.com>
2013-10-10 13:58:14 +0100

    player.name and player.score added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,10 +3,14 @@
 'use strict';
 
 
+
 angular.module('MonkeysApp').factory('api', function(){
         var players = [];
         return{
-            addPlayer: function(player){
+            addPlayer: function(playerName){
+                var player = {};
+                player.name = playerName;
+                player.score = 0;
                 players.push(player);
               },
             getPlayers: function(){
@@ -17,6 +21,12 @@ angular.module('MonkeysApp').factory('api', function(){
 
 angular.module('MonkeysApp').controller('MainCtrl', function ($scope, api) {
         $scope.players = api.getPlayers();
+
+        $scope.currentPlayer = 0;
+
+        $scope.nextPlayer = function(){
+             $scope.currentPlayer ++;
+        }
 
         $scope.addPlayer = function(player){
             api.addPlayer(player);
@@ -41,6 +51,9 @@ angular.module('MonkeysApp').controller('MainCtrl', function ($scope, api) {
         $scope.$on('makey', function(event, value) {
           $scope.monkeys[value].state = !$scope.monkeys[value].state;
           $scope.$apply();
+          $scope.players[$scope.currentPlayer].score += 100;
+
+          log("Score: " + $scope.players[$scope.currentPlayer].score);
         });
 
         $scope.$on('ret_key', function(event, player) {
